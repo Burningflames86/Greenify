838ce49ba664e7ff9d573c149527f58547e637f8
Burningflames86 <110281727+Burningflames86@users.noreply.github.com>
2024-08-14 14:01:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,8 @@ const syne = Syne({ subsets: ["latin"] });
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800"],
+  display: "swap",
+  adjustFontFallback: false,
 });
 const oswald = Oswald({
   subsets: ["latin"],
